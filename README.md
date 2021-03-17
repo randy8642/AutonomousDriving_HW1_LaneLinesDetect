681e8f07b012dbef201a9d9cb0adc2ca5f09dafc
randy8642 <randy8642@gmail.com>
2021-03-17 23:31:42 +0800

    20210317

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AutonomousDriving_HW1_LaneLinesDetect
 NCKU Practices of Autonomous Driving course homework
+
+## 目標
+車道邊線偵測
+
+## 影片素材
+[國道一號 中山高速公路 北向 高雄-基隆 374K-0K 全程 路程景National Highway No. 1](https://www.youtube.com/watch?v=0crwED4yhBA)
+
+## 作法
--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import cv2
 import numpy as np
+
+
+def main():
+
+    frames = getFrame('./data/video.mp4', 4)
+
+    print(frames.shape)
+
+
+def getFrame(videoPath, num):
+
+    cap = cv2.VideoCapture(videoPath)
+    
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
+
+    frames = np.zeros([0, height, width, 3])
+    cnt = 0
+    while(cnt < num):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        frames = np.concatenate(
+            (frames, np.expand_dims(frame, axis=0)), axis=0)
+        cnt += 1
+
+    return frames
+
+
+if __name__ == '__main__':
+    main()
